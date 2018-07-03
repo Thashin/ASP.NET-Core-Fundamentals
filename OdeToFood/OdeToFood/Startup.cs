3b979063e96709651dc48231184b4133c6c813aa
Thashin Naidoo <Thashin@live.com>
2018-07-03 18:26:51 +0800

    Initial commit

---
+++ OdeToFood/OdeToFood/Startup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace OdeToFood
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        //This is a place to register services for dependency injection
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //The service you want to provide and how to instantiate the object
+            services.AddSingleton<IGreeter, Greeter>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //Configures the http processing pipeline, for every request this defines how to respond to that request
+        //The parameters in this method is a type of dependency injection. ASP.NET use the parameters and will pass in an
+        //object or service that implements those interfaces
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IGreeter greeter)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                var greeting = greeter.GetMessageOfTheDay();
+                await context.Response.WriteAsync(greeting);
+            });
+        }
+    }
+}
